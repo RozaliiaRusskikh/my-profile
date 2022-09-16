e3e1384b86268c816e0c32e44e77d72ff9918b64
@roza <rozaliia@intergalactic.com>
2022-09-15 23:16:18 -0700

    Header was added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,2 +1,26 @@
-const Home = () => ('Hello')
+import { Link } from 'react-router-dom';
+import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHand } from '@fortawesome/free-solid-svg-icons';
+
+const Home = () => {
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            {' '}
+            <FontAwesomeIcon icon="fa-regular fa-hand-wave" />
+            Hey, <FontAwesomeIcon icon={faHand} color="	#87CEEB" />
+            <br /> I'm Rozaliia,
+          </h1>
+          <h2>QA Engineer</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 export default Home;
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import Sidebar from '../Sidebar';
-import { Outlet } from 'react-router-dom';
 import './index.scss';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
@@ -8,6 +8,9 @@ const Layout = () => {
       <Sidebar />
       <div className="page">
         <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        
         <span className="tags bottom-tags">
           &lt;/body&gt;
           <br />
