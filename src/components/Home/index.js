2cc5d51f169d63b69d5bca5b9175a6750d041b7f
@roza <rozaliia@intergalactic.com>
2022-10-03 21:58:50 -0700

    Bounced letters - final fixes

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,22 +3,67 @@ import './index.scss';
 import Logo from './Logo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHand } from '@fortawesome/free-solid-svg-icons';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '/Users/roza/my-profile/src/components/AnimatedLetters';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const nameArray = ['R', 'o', 'z', 'a', 'l', 'i', 'i', 'a'];
-  const jobArray = ['J', 'u', 'n', 'i', 'o', 'r', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+  const nameArray = ['R', 'o', 'z', 'a', 'l', 'i', 'i', 'a', ','];
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ];
+
+  useEffect(() => {
+    let timeout;
+
+    timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <>
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-            {' '}
-            Hey, <FontAwesomeIcon icon={faHand} color="#8d8d8d" />
-            <br /> I'm Rozaliia
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _11`}>e</span>
+            <span className={`${letterClass} _12`}>y</span>
+            <span className={`${letterClass} _13`}>,&nbsp;</span>
+            <FontAwesomeIcon icon={faHand} color="#8d8d8d" />
+            <br />
+            <span className={`${letterClass} _14`}>I</span>
+            <span className={`${letterClass} _15`}>'m&nbsp;</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={16}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={24}
+            />
           </h1>
-          <h2>QA Engineer / Junior Developer</h2>
+          <h2>QA Engineer / Developer</h2>
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
