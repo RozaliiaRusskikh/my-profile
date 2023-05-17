8ab89a3bd7f1c6ba7fdea875a0a1d42cd06fc29f
Roza Russkikh <rrrusskikh@gmail.com>
2023-05-17 11:38:29 -0700

    Add all projects manually

--- src/components/MyWork/index.js
+++ src/components/MyWork/index.js
@@ -3,10 +3,16 @@ import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import ProjectCard from '../ProjectCard';
 import travelApp from '../../assets/images/my-travel-app.png';
+import movieApp from '../../assets/images/movie.png';
+import wizardApp from '../../assets/images/wizard.png';
+import spaceApp from '../../assets/images/my-learning-space.png';
 
 const About = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  let travelBookLink = 'https://github.com/RozaliiaRusskikh/my-travel-book';
+  let travelBookLink = 'https://my-travel-memory-book.netlify.app/';
+  let learningSpaceLink = 'https://my-learning-space-app.netlify.app';
+  let movieLink = 'https://smart-movie-engine.netlify.app';
+  let wizardLink = 'https://hogwarts-houses.netlify.app';
 
   useEffect(() => {
     let timeout;
@@ -37,10 +43,31 @@ const About = () => {
           rel="noreferrer"
           href={travelBookLink}
         >
-          <ProjectCard
-            image={travelApp}
-            title="My Travel Book"
-          />
+          <ProjectCard image={travelApp} title="My Travel Book" />
+        </a>
+        <a
+          className="card-link"
+          target="_blank"
+          rel="noreferrer"
+          href={movieLink}
+        >
+          <ProjectCard image={movieApp} title="Smart Movie Engine" />
+        </a>
+        <a
+          className="card-link"
+          target="_blank"
+          rel="noreferrer"
+          href={learningSpaceLink}
+        >
+          <ProjectCard image={spaceApp} title="My Learning Space" />
+        </a>
+        <a
+          className="card-link"
+          target="_blank"
+          rel="noreferrer"
+          href={wizardLink}
+        >
+          <ProjectCard image={wizardApp} title="Find your wizard family" />
         </a>
       </div>
     </div>
